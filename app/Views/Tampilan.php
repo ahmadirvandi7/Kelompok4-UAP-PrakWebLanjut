638531de8e33306484cd7f816ff61b16083564b6
Rafli Ashari Junaidi <92414621+RafliAshariJunaidi@users.noreply.github.com>
2023-11-13 16:22:10 +0700

    pesan commit

--- app/Views/Tampilan.php
+++ app/Views/Tampilan.php
@@ -13,7 +13,7 @@
       src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
       defer
     ></script>
-    <script src="./assets/js/init-alpine.js"></script>
+    <script src="<?=base_url('assets/js/init-alpine.js')?>"></script>
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
@@ -22,8 +22,8 @@
       src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
       defer
     ></script>
-    <script src="./assets/js/charts-lines.js" defer></script>
-    <script src="./assets/js/charts-pie.js" defer></script>
+    <script src="<?=base_url('assets/js/charts-lines.js')?>" defer></script>
+    <script src="<?=base_url('assets/js/charts-pie.js')?>" defer></script>
   </head>
   <body>
     <div
