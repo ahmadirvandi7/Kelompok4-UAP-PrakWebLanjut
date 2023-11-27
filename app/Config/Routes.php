6fbbd7599d441448e2569589c2f51ccd3ebe061a
Rafli Ashari Junaidi <92414621+RafliAshariJunaidi@users.noreply.github.com>
2023-11-27 21:57:56 +0700

    commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,26 +12,26 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::login');
 
 $routes->get('/register','Home::register');
-$routes->get('/tampilan', [Index::class, 'index']);
-$routes->get('/profile', [Index::class, 'profile']);
+$routes->get('/tampilan', [Index::class, 'index'], ['filter' => 'role:dosen']);
+$routes->get('/profile', [Index::class, 'profile'], ['filter' => 'role:dosen']);
 $routes->get('/kelompok', [Index::class,'kelompok']);
-$routes->get('/admin', [Index::class, 'admin']);
+$routes->get('/admin', [Index::class, 'admin'], ['filter' => 'role:admin']);
 
-$routes->get('/frommahasiswa', [Index::class, 'frommahasiswa']);
-$routes->post('/frommahasiswa/store', [Index::class, 'store']);
+$routes->get('/frommahasiswa', [Index::class, 'frommahasiswa'], ['filter' => 'role:mahasiswa']);
+$routes->post('/frommahasiswa/store', [Index::class, 'store'], ['filter' => 'role:mahasiswa']);
 $routes->get('/list_user', [Index::class, 'list_user']);
-$routes->get('/mahasiswa', [Index::class, 'index']);
+$routes->get('/mahasiswa', [Index::class, 'index'], ['filter' => 'role:mahasiswa']);
 $routes->get('/laporan_kegiatan', [Index::class, 'laporan_kegiatan']);
 $routes->get('/pengumuman', [PengumumanController::class, 'index']);
 
 
 
-$routes->get('/datamhs_admin', [Index::class, 'datamhs_admin']);
+$routes->get('/datamhs_admin', [Index::class, 'datamhs_admin'], ['filter' => 'role:admin']);
 $routes->get('/laporankegiatan_admin', [Index::class, 'laporankegiatan_admin']);
 $routes->get('/laporanpelaksanaan_admin', [Index::class, 'laporanpelaksanaan_admin']);
 $routes->get('/datadosenpembimbing_admin', [Index::class, 'datadosenpembimbing_admin']);
-$routes->get('/home_admin', [Index::class, 'home_admin']);
-$routes->get('/formlaporankegiatan', [Index::class, 'formlaporankegiatan']);
+$routes->get('/home_admin', [Index::class, 'home_admin'], ['filter' => 'role:admin']);
+$routes->get('/formlaporankegiatan', [Index::class, 'formlaporankegiatan'], ['filter' => 'role:admin']);
 
-$routes->get('/berandamahasiswa', [Index::class, 'berandamahasiswa']);
+$routes->get('/berandamahasiswa', [Index::class, 'berandamahasiswa'], ['filter' => 'role:mahasiswa']);
 
